d425cfa0d9ab852c216595375ac5dac1632b4fcb
Hyder SM <hydersmh@gmail.com>
2015-12-20 13:25:53 -0800

    added another solution to q_4_4

--- python/src/q_4_4.py
+++ python/src/q_4_4.py
@@ -1,5 +1,6 @@
 from lib.HSTree import BinaryTreeNode
 from lib.HSLinkedList import *
+from queue import Queue
 
 #list: array of linkedlists
 def __createLevelLinkedLists(root, lists, level):
@@ -18,3 +19,24 @@ def createLevelLinkedLists(root):
     lists = []
     __createLevelLinkedLists(root, lists, 0)
     return lists
+
+def createLevelLinkedListsBFS(root):
+    q = Queue()
+    q.put((root, 0))
+    lists = []
+
+    while not q.empty():
+        cur, level = q.get()
+        if level == len(lists):
+            lists.append(Node(cur.key))
+        else:
+            lists[level].appendToTail(cur.key)
+
+        if cur.left:
+            q.put((cur.left, level + 1))
+        if cur.right:
+            q.put((cur.right, level + 1))
+
+    return lists
+        
+
--- python/src/test_q_4_4.py
+++ python/src/test_q_4_4.py
@@ -13,8 +13,11 @@ class testcase1(unittest.TestCase):
         for test in self.tests:
             treeRoot = BinaryTreeNode.buildTree(test[0], test[1])
             levelLists = createLevelLinkedLists(treeRoot)
-            for i in range(len(levelLists)):
+            levelLists2 = createLevelLinkedListsBFS(treeRoot)
+
+            for i in range(len(test[2])):
                 self.assertEqual(str(test[2][i]), str(levelLists[i]))
+                self.assertEqual(str(test[2][i]), str(levelLists2[i]))
 
 if __name__ == "__main__":
    unittest.main() 
