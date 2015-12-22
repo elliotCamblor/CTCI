f1c7c1505135c49cb7df8128f8178d1cf5a38d90
Hyder SM <hydersmh@gmail.com>
2015-12-22 04:49:41 -0500

    changing the verbosity of test_all

--- python/src/test_all.py
+++ python/src/test_all.py
@@ -2,5 +2,5 @@ import unittest
 
 if __name__ == "__main__":
     tests = unittest.TestLoader().discover('.')
-    testRunner = unittest.runner.TextTestRunner()
+    testRunner = unittest.runner.TextTestRunner(verbosity=2)
     testRunner.run(tests)
