644f87aaca73e8955c15a777fa5d2d23001b0d41
Hyder SM <hydersmh@gmail.com>
2016-01-11 14:24:16 -0500

    updated q_9_3

--- python/src/q_9_3.py
+++ python/src/q_9_3.py
@@ -20,3 +20,23 @@ def __magicFast(a, start, end):
 
 def magicFast(a):
     return __magicFast(a, 0, len(a) - 1)
+
+def __magicFastDuplicates(a, start, end):
+    if end < start:
+        return -1
+
+    mid = (start + end) // 2
+    midVal = a[mid]
+
+    if mid == midVal:
+        return mid
+
+    r = __magicFastDuplicates(a, start, min(mid - 1, midVal))
+    if r > 0:
+        return r
+
+    r = __magicFastDuplicates(a, max(mid + 1, midVal) , end) 
+    return r
+
+def magicFastDuplicates(a):
+    return __magicFastDuplicates(a, 0, len(a) - 1)
--- python/src/test_q_9_3.py
+++ python/src/test_q_9_3.py
@@ -6,11 +6,19 @@ class testcase1(unittest.TestCase):
         ([-20, 0, 2, 5, 7, 8, 20], 2),
         ([0], 0)
     ]
+
+    testsDuplicates = [
+        ([-3, 2, 2, 2, 4, 10], 2)
+    ]
     
     def test(self):
         for test in self.tests:
             self.assertEqual(magicBrute(test[0]), test[1])
             self.assertEqual(magicFast(test[0]), test[1])
+            self.assertEqual(magicFastDuplicates(test[0]), test[1])
+
+        for test in self.testsDuplicates:
+            self.assertEqual(magicFastDuplicates(test[0]), test[1])
 
 if __name__ == "__main__":
     unittest.main() 
